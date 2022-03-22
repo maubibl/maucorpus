4f6a2f7e1e76d092a9e34480310431d866f8a3d5
Markus Skyttner <markussk@kth.se>
2022-03-22 15:34:24 +0100

    add missing file

--- data-raw/kth_besta.R
+++ data-raw/kth_besta.R
@@ -1,8 +1,11 @@
 kth_besta <- readr::read_csv("data-raw/kth_besta.csv")
 
 #readr::write_csv(kth_besta, "data-raw/kth_besta.csv", na = "")
+
 #kth_besta <-
 #  kth_besta %>%
 #  select(emp_code = Kod, emp_title_sv = Befattning, emp_title_en = Position)
 
 usethis::use_data(kth_besta, overwrite = TRUE)
+
+# system("mc cp data-raw/kth_besta.csv kthb/kthcorpus/kth_besta.csv")

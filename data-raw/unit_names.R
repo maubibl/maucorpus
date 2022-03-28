c4b0878addc05b49c3e2dce215d9e2afe5d17562
Markus Skyttner <markussk@kth.se>
2022-03-28 17:40:49 +0200

    document and add data types to unit_names, and harmonize column names

--- data-raw/unit_names.R
+++ data-raw/unit_names.R
@@ -1,5 +1,18 @@
-unit_names <- readr::read_csv("data-raw/unit_names.csv", show_col_types = FALSE)
+library(readr)
 
-# readr::write_csv(unit_names, "data-raw/unit_names.csv")
+# data types
+ct <- cols(
+ .default = col_character(),
+ is_closed = col_logical(),
+ level = col_integer(),
+ orgid = col_integer()
+)
+
+unit_names <-
+  readr::read_csv("data-raw/unit_names.csv", show_col_types = FALSE, col_types = ct)
+
+# readr::write_csv(unit_names, "data-raw/unit_names.csv", na = "")
 
 usethis::use_data(unit_names, overwrite = TRUE)
+
+#system("mc cp data-raw/unit_names.csv kthb/kthcorpus")

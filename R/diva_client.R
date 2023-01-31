193b4d1251a88ff8101bfd9970437c0497f5e1df
Markus Skyttner <markussk@kth.se>
2023-01-31 20:25:15 +0100

    add fcn and gha for uploading orcid kthid pairs

--- R/diva_client.R
+++ R/diva_client.R
@@ -810,3 +810,63 @@ diva_refresh_trigger <- function() {
   return(invisible(TRUE))
 }
 
+#' Upload DiVA ORCiD kthId pairs to object storage
+#'
+#' This function can be used to automatically download data from DiVA
+#' from scratch, render the report and upload it to an S3 bucket
+#' @export
+#' @importFrom readr write_csv
+diva_orcid_kthid_upload <- function() {
+
+  # upload to bucket "kthcorpus"
+  upload <- diva_orcid_kthid()
+  readr::write_csv(upload, "/tmp/diva_kthid_orcid.csv")
+  diva_upload_s3(path = "/tmp/diva_kthid_orcid.csv", "diva_kthid_orcid.csv")
+}
+
+diva_orcid_kthid <- function() {
+
+  aut <-
+    kth_diva_authors() |> select(name, orcid, kthid, PID) |>
+    filter(!is.na(kthid), !is.na(orcid), grepl("^u1.{6}$", kthid))
+
+  # orcids with one or more kthids
+  diva_orcid_kthids <-
+    aut |> group_by(orcid) |>
+    summarize(.groups = "rowwise",
+      kthid = unique(kthid),
+      author = paste(collapse = "|", unique(trimws(name))),
+      n_kthid = n_distinct(kthid),
+      n_pubs = n_distinct(PID)
+    ) |>
+    arrange(desc(n_kthid), desc(n_pubs), orcid)
+
+  # kthids with one or more orcids
+  diva_kthid_orcids <-
+    aut |> group_by(kthid) |>
+    summarize(.groups = "rowwise",
+      orcid = unique(orcid),
+      author = paste(collapse = "|", unique(trimws(name))),
+      n_orcid = n_distinct(orcid),
+      n_pubs = n_distinct(PID)
+    ) |>
+    arrange(desc(n_orcid), desc(n_pubs), kthid)
+
+  # NB: if we look for kthids with more than 1 orcids, we get 397 rows
+  #diva_kthid_orcids |> filter(n_orcid > 1)
+
+  # NB: if we look for orcids with more than 1 kthids, we get 114 rows
+  #diva_orcid_kthids |> filter(n_kthid > 1)
+
+  # kthid and orcid pairs with one-to-one relationships
+  res <-
+      diva_kthid_orcids |> filter(n_orcid == 1) |>
+      inner_join(by = c("kthid", "orcid", "author", "n_pubs"),
+        diva_orcid_kthids |> filter(n_kthid == 1)
+      ) |>
+      select(orcid, kthid, n_pubs)
+
+  return(res)
+
+
+}

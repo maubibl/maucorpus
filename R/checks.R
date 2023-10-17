1aaf7c6dc3335878adc5d63a6e9d85067780cce2
awandahl <aw@kth.se>
2023-10-17 14:41:12 +0000

    Added preprint DOI-prefixes for OSF and Preprints.org

--- R/checks.R
+++ R/checks.R
@@ -973,12 +973,14 @@ check_manuscripts_with_identifiers <- function(pubs = kth_diva_pubs()) {
   # DOIs are usually for published works, sometimes pre-prints provide DOIs though
 
   # Suppress false positives by excluding DOIs starting with
-  # 10.1101 and 10.48550 (bioRxiv, arXiv and SSRN)
+  # 10.1101, 10.48550, 10.2139, 10.31219, 10.20944 (bioRxiv, arXiv, SSRN, OSF, Preprints.org)
   # https://doi.org/10.1101/2020.08.24.252296
   # https://doi.org/10.48550/ARXIV.2202.11577
   # https://doi.org/10.2139/ssrn.4385563
+  # https://doi.org/10.31219/osf.io/mgjwv
+  # https://doi.org/10.20944/preprints202101.0605.v1
 
-  re_exclude <- "^10.1101|^10.48550|^10.2139" # bioRxiv,arXiv and SSRN
+  re_exclude <- "^10.1101|^10.48550|^10.2139|^10.31219|^10.20944" # see above
 
   ScopusId <- PMID <- Year <- LastUpdated <- NULL
 

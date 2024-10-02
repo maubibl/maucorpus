af344b44d78be23d05ee47f334780ec74724d451
Markus Skyttner <mskyttner@pm.me>
2024-10-02 10:52:53 +0200

    skip some tests on ci and remove redundant file

--- tests/testthat/test-oai-pmh.R
+++ tests/testthat/test-oai-pmh.R
@@ -1,19 +1,21 @@
 test_that("connection through OAI-PMH can be made", {
+  skip_on_ci()
   hello <- oai_hello()
   is_valid <- nrow(hello$sets) > 20 
   expect_true(is_valid)
 })
 
 test_that("database exists w harvest", {
-
+  skip_on_ci()
   con <- oai_con()
   on.exit(DBI::dbDisconnect(con))
   is_valid <- "ids" %in% DBI::dbListTables(con)
   expect_true(is_valid)
-
 })
 
 test_that("changes since latest timestamp from database can be retrieved", {
+
+  skip_on_ci()
 
   changes <- oai_db_refresh()
   n_rows <- changes |> map_int(nrow)
